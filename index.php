4b47dfcbb877c8cc3ce32aaa046b6cb959014a58
Hernan <hernanbelizan@gmail.com>
2019-05-16 03:22:09 -0300

    Agrego require y knockout

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bower_components/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    
+    <script src="bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="bower_components/requirejs/require.js" type="text/javascript"></script>
+    <script src="bower_components/knockout/knockout.js" type="text/javascript"></script>
     <title>Pag - Test</title>
 </head>
 <body>
-    
+    <h1>Hola Mundo</h1>
 </body>
 </html>
